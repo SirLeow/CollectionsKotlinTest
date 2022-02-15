9ba49ced6372f3bff39e6bf4851159f6552cc27c
Thiago Barros <t.b.lima18@gmail.com>
2022-02-15 11:37:47 -0300

    Created a class to test the intArrayOf function

---
+++ src/main/kotlin/TestIntArrayOf.kt
@@ -0,0 +1,25 @@
+import kotlin.random.Random
+
+fun main() {
+    val divider = "--------------------------------"
+    val values = intArrayOf(
+        Random.nextInt(0,100),
+        Random.nextInt(0,100),
+        Random.nextInt(0,100),
+        Random.nextInt(0,100),
+        Random.nextInt(0,100),
+        Random.nextInt(0,100)
+    )
+
+    values.forEach { println(it) }
+    println(divider)
+
+    for (i in values) println(i)
+    println(divider)
+
+    values.sort()
+    for (i in values) println(i)
+    println(divider)
+
+
+}
