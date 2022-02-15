9b929405b5227bd0760449cc30e901b32d946917
Thiago Barros <t.b.lima18@gmail.com>
2022-02-15 13:38:10 -0300

    Created a class to test the DoubleArray function

---
+++ src/main/kotlin/TestDoubleArray.kt
@@ -0,0 +1,21 @@
+import kotlin.random.Random
+
+fun main() {
+    val divider = "--------------------------------"
+    val salarios = DoubleArray(5)
+    salarios.forEachIndexed { index, _ -> salarios[index] = Random.nextInt(100,10000).toDouble() }
+
+    salarios.forEach { println(it) }
+    println(divider)
+
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario *1.5
+    }
+
+    for (i in salarios) println(i)
+    println(divider)
+
+    salarios.sort()
+    salarios.forEach { println(it) }
+
+}
