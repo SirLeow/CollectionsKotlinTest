a5e65331e58007d4ea5de7cf91812a504b553b92
Thiago Barros <t.b.lima18@gmail.com>
2022-02-16 12:32:26 -0300

    Created a class to test the Set function

---
+++ src/main/kotlin/TestSet.kt
@@ -0,0 +1,41 @@
+import kotlin.random.Random
+
+fun main() {
+    val div = "--------------------"
+    val funcionarios = setOf(insert.sother,
+        insert.aseyoi,
+        insert.rewigi,
+        insert.bia,
+        insert.rebeca,
+        insert.cheetos,
+        insert.debora,
+        insert.juan
+    )
+
+    val group1 = (funcionarios.filter {it.senha == Random.nextInt(1,5)}).toSet()
+    val group2 = (funcionarios.filter {it.senha == Random.nextInt(1,5)}).toSet()
+    val union = group1.union(group2)
+    val subtract = funcionarios.subtract(group1)
+    val intersect = funcionarios.intersect(group2)
+
+    println("Funcionarios")
+    funcionarios.forEach{ println(it) }
+    println(div)
+    println("Group 1")
+    group1.forEach{ println(it) }
+    println(div)
+    println("Group 2")
+    group2.forEach{ println(it) }
+    println(div)
+    println("Union")
+    union.forEach{ println(it) }
+    println(div)
+    println("Subtract")
+    subtract.forEach{ println(it) }
+    println(div)
+    println("Intersect")
+    intersect.forEach{ println(it) }
+    println(div)
+
+}
+
