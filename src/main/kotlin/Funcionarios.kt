72f854f7fb7514af8f8c02e53d4dbb4e87d745a2
Thiago Barros <t.b.lima18@gmail.com>
2022-02-16 12:31:11 -0300

    Added declaration of the variables and changed tipo to senha

--- src/main/kotlin/Funcionarios.kt
+++ src/main/kotlin/Funcionarios.kt
@@ -1,10 +1,24 @@
+import kotlin.random.Random
+
 data class Funcionario(
     val nome:String,
     val salario:Double,
-    val tipo:String
+    val senha:Int
 ){
     override fun toString(): String = """
-        Nome: $nome
-        Salario: $salario        
+        Nome: $nome - Salario: $salario - Senha: $senha        
     """.trimIndent()
+}
+
+class insert{
+    companion object {
+        val sother   = Funcionario(nome = "Sother ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val aseyoi   = Funcionario(nome = "Aseyoi ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val rewigi   = Funcionario(nome = "Rewigi ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val bia      = Funcionario(nome = "Bia    ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val rebeca   = Funcionario(nome = "Rebeca ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val cheetos  = Funcionario(nome = "Cheetos", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val debora   = Funcionario(nome = "Debora ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+        val juan     = Funcionario(nome = "Juan   ", salario = Random.nextInt(1000, 10000).toDouble(), senha = Random.nextInt(1,5))
+    }
 }
