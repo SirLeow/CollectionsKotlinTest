2a945c62d654ecd39688e46a289b8ec7329f9b8d
Thiago Barros <t.b.lima18@gmail.com>
2022-02-17 19:18:38 -0300

    Created a class to simulate a server(Repositorio) and a class to test the mutableMap function

---
+++ src/main/kotlin/Repositorio.kt
@@ -0,0 +1,13 @@
+
+class Repositorio<k>{
+    private val map = mutableMapOf<Int, k>()
+
+    fun create(id: Int, value: k){
+        map[id] = value
+    }
+
+    fun remove(id:Int) = map.remove(id)
+    fun findById(id:Int) = map[id]
+    fun findAll() = map.values
+
+}
---
+++ src/main/kotlin/TestMutableMap.kt
@@ -0,0 +1,26 @@
+import kotlin.random.Random
+
+fun main() {
+
+    val repositorio = Repositorio<Funcionario>()
+
+    listaFuncionarios.forEachIndexed {index, s ->
+        repositorio.create(
+            id = index,
+            Funcionario(nome = s,
+                        salario = Random.nextInt(1000,10000).toDouble(),
+                        senha = Random.nextInt(1,5)
+            )
+        )
+    }
+
+    for(i in listaFuncionarios.indices){
+        println(repositorio.findById(id = i))
+    }
+    println(div)
+    repositorio.findAll().forEach{ println(it) }
+
+    println(div)
+    repositorio.remove(Random.nextInt(0, listaFuncionarios.size-1))
+    repositorio.findAll().forEach{ println(it) }
+}
