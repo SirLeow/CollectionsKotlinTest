ac5cd6401be18a0c2944f95ffcb23ccbd0077086
Thiago Barros <t.b.lima18@gmail.com>
2022-02-17 19:53:38 -0300

    Created a class with extensions to the Array function and a class to test it

---
+++ src/main/kotlin/TestExtensionFunction.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val notas = arrayOf(
+        "4".toBigDecimal(),
+        "6".toBigDecimal(),
+        "7".toBigDecimal(),
+        "10".toBigDecimal(),
+        "9".toBigDecimal()
+    )
+
+    println(notas.somatoria())
+    println(div)
+    println(notas.media())
+}
